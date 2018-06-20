3e5789c8318a34bb744b2d4e040ae183f871126f
kirill <kirillmoiseenkov@mail.ru>
2018-06-20 15:09:04 +0400

    remove some classes

--- src/main/java/CreationShip/demo/DemoApplication.java
+++ src/main/java/CreationShip/demo/DemoApplication.java
@@ -1,15 +1,11 @@
 package CreationShip.demo;
 
 import CreationShip.demo.IO.Acceptor;
-import CreationShip.demo.IO.AcceptorNIO;
 import CreationShip.demo.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-
-import java.util.concurrent.Executor;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner{
